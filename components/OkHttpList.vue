32f786ac0e301a6e1a3d0503308dd100347d7c54
Joel Hernandez <joel@open-book.org>
2020-02-12 16:51:55 +0100

    :construction: prototype OBPostsStream

---
+++ components/OkHttpList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from "nuxt-property-decorator"
+
+    @Component({
+        name: "OkHttpList",
+    })
+    export default class extends Vue {
+
+    }
+</script>
---
+++ components/posts-stream/OkPostsStream.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from "nuxt-property-decorator"
+
+    @Component({
+        name: "OkPostsStream",
+        props: {
+
+        }
+    })
+    export default class extends Vue {
+
+        @Prop(Function) readonly refresher: Function | undefined;
+
+        @Prop(Function) readonly onScrollLoader: Function | undefined;
+
+        @Prop(Function) readonly onScrollLoader: Function | undefined;
+
+        @Prop(Function) readonly onStatusChanged: Function | undefined;
+
+        private emitPostsRefreshed(){
+            this.$emit('onPostsRefreshed');
+        }
+
+        private emitStatusChanged(){
+            this.$emit('onStatusChanged');
+        }
+
+
+    }
+</script>
