5406182e43263a44d37dab0a51996b0b3f5b71aa
Joel Hernandez <joel@open-book.org>
2020-07-01 22:30:32 +0200

    :bug: fix Circle model does not serialize color

---
+++ components/tiles/grouped-actions/connect-with-user/components/OkUpdateConnectionCirclesTile.vue
@@ -0,0 +1,67 @@
+<template>
+    <ok-tile :on-click="onWantsToReportUser">
+        <template v-slot:leading>
+            <ok-report-icon
+                    class="ok-svg-icon-primary-invert"></ok-report-icon>
+        </template>
+
+        <template v-slot:content>
+                    <span class="ok-has-text-primary-invert">
+                                {{$t('global.snippets.report_user')}}
+                            </span>
+        </template>
+    </ok-tile>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from "nuxt-property-decorator"
+    import OkTile from "~/components/tiles/OkTile.vue";
+    import { IModalService } from "~/services/modal/IModalService";
+    import { okunaContainer } from "~/services/inversify";
+    import { TYPES } from "~/services/inversify-types";
+    import { IToastService } from "~/services/toast/IToastService";
+    import { ILocalizationService } from "~/services/localization/ILocalizationService";
+    import { ToastType } from '~/services/toast/lib/ToastType';
+    import { IUser } from '~/models/auth/user/IUser';
+
+    @Component({
+        name: "OkUpdateConnectionCirclesTile",
+        components: {OkTile},
+    })
+    export default class OkUpdateConnectionCirclesTile extends Vue {
+
+        @Prop({
+            type: Object,
+            required: false
+        }) readonly user: IUser;
+
+
+        @Prop({
+            type: Function,
+            required: false
+        }) readonly onUserReported: (user: IUser) => void;
+
+        private modalService: IModalService = okunaContainer.get<IModalService>(TYPES.ModalService);
+        private toastService: IToastService = okunaContainer.get<IToastService>(TYPES.ToastService);
+        private localizationService: ILocalizationService = okunaContainer.get<ILocalizationService>(TYPES.LocalizationService);
+
+
+        onWantsToReportUser() {
+            this.modalService.openReportObjectModal({
+                object: this.user,
+                onObjectReported: (user: any) => {
+                    this.toastService.show({
+                        type: ToastType.success,
+                        message: this.localizationService.localize("global.snippets.user_reported")
+                    });
+                    this.$emit("onUserReported", user);
+                    if(this.onUserReported) this.onUserReported(user);
+                }
+            });
+        }
+    }
+</script>
