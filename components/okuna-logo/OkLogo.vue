aa07f476f5095fe713b5ce08f387c826a96cb90d
Sebastian peralta <juan.peralta@unillanos.edu.co>
2020-11-10 10:04:50 -0500

    change logo

--- components/okuna-logo/OkLogo.vue
+++ components/okuna-logo/OkLogo.vue
@@ -2,17 +2,18 @@
     <nuxt-link :to="localePath('index')" class="is-size-4 is-flex align-items-center has-no-hover-text-decoration">
         <img :src="logoImage" class="ok-logo" alt="Okuna logo">
         <div class="ok-logo--text has-text-logo is-size-5 ok-has-text-primary-invert is-hidden-mobile has-padding-left-10 is-flex align-items-center justify-center" :class="textClass" style="padding-top: 5px">
-            {{$t('name')}} <span class="ok-has-text-primary-invert-60 is-size-6">&nbsp; | WIP</span>
+           <!--  {{$t('name')}} <span class="ok-has-text-primary-invert-60 is-size-6">&nbsp; | WIP</span> -->
         </div>
     </nuxt-link>
 </template>
 
 <style lang="scss" scoped>
     $header-size: 1.7rem;
+    $width-size: 5.7rem;
 
     .ok-logo {
         height: $header-size;
-        width: $header-size;
+        width: $width-size;
     }
 
 </style>
