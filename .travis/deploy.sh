da006b00ce4ffc579b124dace1d114c50353f921
coac-mahnoor <mahnoor.baloch@coac.de>
2021-02-03 23:55:35 +0500

    modified travis deployment over https

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,16 +3,7 @@
 # start the ssh agent
 eval "$(ssh-agent -s)"
 
-# set required key permissions
-chmod 600 .travis/dokku.key
-
-# add dokku key to ssh
-ssh-add .travis/dokku.key
-
-# add dev server to known hosts
-ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts
-
-git remote add dokku $DOKKU_GIT_REMOTE
+git remote add deploy https://dev.coac.de/okuna-web.git
 
 # force push to dokku master from develop branch
-git push -f dokku hydrogen:master
+git push -f deploy hydrogen:master
